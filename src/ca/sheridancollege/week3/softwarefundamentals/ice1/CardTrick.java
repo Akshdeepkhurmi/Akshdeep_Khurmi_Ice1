8cab868e5df0584941b0ae584128d47bb6e55213
NL-Ams <NL-Ams@192.168.2.99>
2023-05-26 10:12:44 -0400

    cards trick

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,47 +5,58 @@ public class CardTrick {
     
     public static void main(String[] args) {
         Card[] magicHand = new Card[7];
+        Random random = new Random();
         
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            c.setValue(generateRandomNumber());
-            c.setSuit(Card.SUITS[generateRandomNumberBetween(0, 3)]);
+            c.setValue(random.nextInt(13) + 1);
+            c.setSuit(Card.SUITS[random.nextInt(4)]);
             magicHand[i] = c;
         }
         
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the value of your card: ");
         int userValue = scanner.nextInt();
-        System.out.print("Enter the suit of your card (0-3): ");
-        int userSuit = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+        System.out.print("Enter the suit of your card (Heart, Diamonds, Spades, Clubs): ");
+        String userSuit = scanner.nextLine();
         
         Card userCard = new Card();
         userCard.setValue(userValue);
-        userCard.setSuit(Card.SUITS[userSuit]);
+        userCard.setSuit(userSuit);
         
-        boolean foundMatch = false;
+        Card luckyCard = new Card();
+        luckyCard.setValue(7); // Set the card value of your choice (1-13)
+        luckyCard.setSuit("Hearts"); // Set the suit of your choice ("Hearts", "Diamonds", "Spades", "Clubs")
+        
+        boolean foundUserCard = false;
+        boolean foundLuckyCard = false;
+        
         for (Card card : magicHand) {
-            if (card.getValue() == userCard.getValue() && card.getSuit().equals(userCard.getSuit())) {
-                foundMatch = true;
+            if (card.getValue() == userCard.getValue() && card.getSuit().equalsIgnoreCase(userCard.getSuit())) {
+                foundUserCard = true;
+            }
+            
+            if (card.getValue() == luckyCard.getValue() && card.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
+                foundLuckyCard = true;
+            }
+            
+            if (foundUserCard && foundLuckyCard) {
                 break;
             }
         }
         
-        if (foundMatch) {
+        if (foundUserCard) {
             System.out.println("Your card was found in the magic hand!");
         } else {
             System.out.println("Your card was not found in the magic hand.");
         }
-    }
-    
-    private static int generateRandomNumber() {
-        Random random = new Random();
-        return random.nextInt(13) + 1;
-    }
-    
-    private static int generateRandomNumberBetween(int min, int max) {
-        Random random = new Random();
-        return random.nextInt(max - min + 1) + min;
+        
+        if (foundLuckyCard) {
+            System.out.println("The lucky card is in the magic hand!");
+        } else {
+            System.out.println("The lucky card is not in the magic hand.");
+        }
     }
     
 }
